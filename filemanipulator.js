a24ec07262307c7623fb126197ba79decb6c1f5a
schoesche <ga@schoesche.ch>
2015-03-04 17:45:23 +0100

    plugin starts in a window

--- filemanipulator.js
+++ filemanipulator.js
@@ -1,107 +1,22 @@
 'use strict';
+alert("gugs");
 /**
  * Created by U116064 on 03.03.2015.
  */
 
 var app = angular.module("Testchromplugin", []);
 
-function alertSomthing() {
+app.controller('filemanipulatorCtrl', ['$scope', function ($scope) {
 
-    alert("gugus");
+    console.log("hallo test 1");
 
-};
+    alert("ahhhhh super angular");
 
-/*function errorHandler(e) {
-    var msg = '';
+    /*window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
 
-    switch (e.code) {
-        case FileError.QUOTA_EXCEEDED_ERR:
-            msg = 'QUOTA_EXCEEDED_ERR';
-            break;
-        case FileError.NOT_FOUND_ERR:
-            msg = 'NOT_FOUND_ERR';
-            break;
-        case FileError.SECURITY_ERR:
-            msg = 'SECURITY_ERR';
-            break;
-        case FileError.INVALID_MODIFICATION_ERR:
-            msg = 'INVALID_MODIFICATION_ERR';
-            break;
-        case FileError.INVALID_STATE_ERR:
-            msg = 'INVALID_STATE_ERR';
-            break;
-        default:
-            msg = 'Unknown Error';
-            break;
-    }
-    ;
-
-    console.log('Error: ' + msg);
-}*/
-
-function onInitFs(fs) {
-    console.log('Opened file system: ' + fs.name);
-}
-
-
-/*function onInitFs(fs) {
-
- fs.root.getFile('logTestLOG.txt', {create: true, exclusive: true}, function(fileEntry) {
-
- // fileEntry.isFile === true
- // fileEntry.name == 'log.txt'
- // fileEntry.fullPath == '/log.txt'
-
- }, errorHandler);
-
- };*/
-app.controller('filemanipulatorCtrl', ['ErrorFactory', function (ErrorFactory) {
-    window.requestFileSystem(window.TEMPORARY, 1024 * 1024, onInitFs, ErrorFactory.errorHandler());
-
-    document.addEventListener('DOMContentLoaded', function () {
-        /*alertSomthing();*/
-
-        window.requestFileSystem(window.TEMPORARY, 5 * 1024 * 1024 /*5MB*/, onInitFs, ErrorFactory.errorHandler());
-    });
-
-
-
-    chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.tabs.create({'url': chrome.extension.getURL('popup.html')}, function(tab) {
-            // Tab opened.
-        });
-    });
+    window.requestFileSystem(type, size, successCallback, opt_errorCallback)*/
 
 
 
 }]);
 
-app.factory('ErrorFactory',[ function(){
-    var factory = {};
-    factory.errorHandler = function(e){
-        var msg = '';
-
-        switch (e.code) {
-            case FileError.QUOTA_EXCEEDED_ERR:
-                msg = 'QUOTA_EXCEEDED_ERR';
-                break;
-            case FileError.NOT_FOUND_ERR:
-                msg = 'NOT_FOUND_ERR';
-                break;
-            case FileError.SECURITY_ERR:
-                msg = 'SECURITY_ERR';
-                break;
-            case FileError.INVALID_MODIFICATION_ERR:
-                msg = 'INVALID_MODIFICATION_ERR';
-                break;
-            case FileError.INVALID_STATE_ERR:
-                msg = 'INVALID_STATE_ERR';
-                break;
-            default:
-                msg = 'Unknown Error';
-                break;
-        };
-        console.log('Error: ' + msg);
-    };
-    return factory;
-}]);
