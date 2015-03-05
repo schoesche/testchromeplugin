c1282631b77e3be4cc8c645ecce8569a84c05f0c
schoesche <ga@schoesche.ch>
2015-03-05 08:19:11 +0100

    add chrome code... error

--- filemanipulator.js
+++ filemanipulator.js
@@ -1,5 +1,4 @@
 'use strict';
-/*alert("gugs");*/
 /**
  * Created by U116064 on 03.03.2015.
  */
@@ -12,11 +11,26 @@ angular.module("Testchromplugin", [])
 
     alert("ahhhhh super angular");
 
-    /*window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+/*    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
 
-    window.requestFileSystem(type, size, successCallback, opt_errorCallback)*/
+    window.requestFileSystem(window.PERSISTENT, 1*1024*1024 *//*1MB*//*, successCallback, opt_errorCallback);
 
 
+        window.webkitStorageInfo.requestQuota(PERSISTENT, 1*1024*1024 *//*1MB*//*, function(grantedBytes) {
+            window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+        }, function(e) {
+            console.log('Error', e);
+        });*/
 
+
+        chrome.fileSystem.chooseEntry({type: 'saveFile'}, function(writableFileEntry) {
+            writableFileEntry.createWriter(function(writer) {
+                writer.onerror = errorHandler;
+                writer.onwriteend = function(e) {
+                    console.log('write complete');
+                };
+                writer.write(new Blob(['1234567890'], {type: 'text/plain'}));
+            }, errorHandler);
+        });
 }]);
 
